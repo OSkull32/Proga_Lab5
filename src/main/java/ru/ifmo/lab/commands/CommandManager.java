22ff64912694a82b205df93c4813881a4a7dd5aa
Kliodt Vadim <vadimkl2004@mail.ru>
2023-02-27 01:21:36 +0300

    Add documentation

--- src/main/java/ru/ifmo/lab/commands/CommandManager.java
+++ src/main/java/ru/ifmo/lab/commands/CommandManager.java
@@ -7,21 +7,38 @@ import ru.ifmo.lab.utility.Console;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Класс, управляющий вызовом команд.
+ * @author Kliodt Vadim
+ * @version 1.0
+ */
 public class CommandManager {
 
     private final Console CONSOLE;
-    private HashMap<String, Command> commands = new HashMap<>();
-    private ArrayList<String> historyList = new ArrayList<>();
-    private final int MAX_HISTORY_SIZE = 13;
+    private final HashMap<String, Command> COMMANDS = new HashMap<>();
+    private final ArrayList<String> HISTORY_LIST = new ArrayList<>();
 
-    public CommandManager(Console console){
+    /**
+     * Конструирует менеджера команд с заданными {@link Console}
+     * @param console Объект {@link Console}, через который класс
+     *                осуществляет взаимодействие с пользователем.
+     */
+    public CommandManager(Console console) {
         this.CONSOLE = console;
     }
 
-    public void addCommand(String name, Command command){
-        commands.put(name, command);
+    /**
+     * Добавляет команду к общему списку и делает ее возможной для вызова.
+     * @param name название команды.
+     * @param command объект класса команды.
+     */
+    public void addCommand(String name, Command command) {
+        COMMANDS.put(name, command);
     }
 
+    /**
+     * При вызове этого метода в консоли запрашивается команда.
+     */
     public void nextCommand() {
         CONSOLE.printPreamble(); //print ">>>"
         String inputString = CONSOLE.readLine();
@@ -38,23 +55,28 @@ public class CommandManager {
         }
     }
 
+    //метод вызывает команду на исполнение
     private void executeCommand(String commandName) throws InvalidCommandException, InvalidValueException {
 
-        Command command = commands.get(commandName);
+        Command command = COMMANDS.get(commandName);
         if (command == null) throw new InvalidCommandException();
         command.execute();
 
-        historyList.add(commandName);
-        if (historyList.size() > MAX_HISTORY_SIZE){
-            historyList.remove(0);
+        HISTORY_LIST.add(commandName);
+
+        if (HISTORY_LIST.size() > 13) {
+            HISTORY_LIST.remove(0);
         }
     }
 
+    /**
+     * Печатает в консоль последние 13 использованных команд.
+     */
     public void getHistoryList() {
-        if (historyList.size() == 0) {
+        if (HISTORY_LIST.size() == 0) {
             CONSOLE.printCommandText("History is empty");
-        } else{
-            CONSOLE.printCommandText(historyList.toString());
+        } else {
+            CONSOLE.printCommandText(HISTORY_LIST.toString());
         }
     }
 }
--- src/main/java/ru/ifmo/lab/commands/History.java
+++ src/main/java/ru/ifmo/lab/commands/History.java
@@ -1,10 +1,25 @@
 package ru.ifmo.lab.commands;
 
+/**
+ * Класс команды "history".
+ * @author Kliodt Vadim
+ * @version 1.0
+ */
 public class History implements Command{
     private final CommandManager COMMAND_MANAGER;
+
+    /**
+     * Конструирует объект, првязывая его к конкретному объекту {@link CommandManager}.
+     * @param commandManager указывет на объект {@link CommandManager}, в котором
+     *                       будет вызываться соответствующий метод {@link CommandManager#getHistoryList()}.
+     */
     public History(CommandManager commandManager){
         this.COMMAND_MANAGER = commandManager;
     }
+
+    /**
+     * Выполняет команду "history".
+     */
     @Override
     public void execute(){
         COMMAND_MANAGER.getHistoryList();
